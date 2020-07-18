be3eaf3c240b09446a1d2267417adf188c12b098
rxsehga <raviseh@gmail.com>
2020-07-18 08:57:32 +0530

    config

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,12 +1,50 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
 pipelineJob('theme-park-job') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/rxsehga/spring-boot-api-example.git'
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
                     }
                     branch 'master'
+                }
+            }
+        }
+    }
+}
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
+pipelineJob('theme-park-job-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-aws')
                 }
             }
         }
